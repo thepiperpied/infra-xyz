833d6881da75c3980720468799738164febaa509
Shreyansh Zazz <shrey.binary@gmail.com>
2020-02-07 15:08:38 +0530

    refactor: extract basic checks and add basicCheckTask to run first (#24)

--- src/tasks/init.task.ts
+++ src/tasks/init.task.ts
@@ -1,10 +1,13 @@
 const taskz = require('taskz')
 import KubectlAPI from '../api/kubectl.api'
+import CoreAPI from '../api/core.api'
 
 export default class InitTask {
   KUBECTL_API = new KubectlAPI()
 
-  INIT_TASK: Array<any> = [this.KUBECTL_API.kubectlTask()]
+  CORE = new CoreAPI()
+
+  INIT_TASK: Array<any> = [this.CORE.basicCheckTask(), this.KUBECTL_API.kubectlTask()]
 
   run() {
     taskz(this.INIT_TASK).run()
